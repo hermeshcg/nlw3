9e5d2dea8d322e48a743581d55cbe0ec50519f74
Hermes Caretta <hermescaretta@gmail.com>
2021-03-02 14:03:35 -0300

    nem lembrava q tinha coisa pra dar commit mano

--- web/src/pages/Orphanage.tsx
+++ web/src/pages/Orphanage.tsx
@@ -1,79 +1,82 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from 'react';
 /* import { FaWhatsapp } from "react-icons/fa"; */
-import { FiClock, FiInfo } from "react-icons/fi";
-import { Map, Marker, TileLayer } from "react-leaflet";
-import {useParams} from 'react-router-dom';
+import { FiClock, FiInfo } from 'react-icons/fi';
+import { Map, Marker, TileLayer } from 'react-leaflet';
+import { useParams } from 'react-router-dom';
 
 import '../styles/pages/orphanage.css';
-import Sidebar from "../components/Sidebar";
- 
-import happyMapIcon from '../utils/mapIcon';
-import api from "../services/api";
+import Sidebar from '../components/Sidebar';
 
-interface Orphanage{
+import happyMapIcon from '../utils/mapIcon';
+import api from '../services/api';
+
+interface Orphanage {
   latitude: number;
   longitude: number;
   name: string;
   about: string;
   instructions: string;
-  opening_hours:  string;
+  opening_hours: string;
   open_on_weekends: boolean;
   images: Array<{
     url: string;
     id: number;
-  }>
+  }>;
 }
 
-interface OrphanageParams{
+interface OrphanageParams {
   id: string;
 }
 
 export default function Orphanage() {
-  const params = useParams<OrphanageParams>()
+  const params = useParams<OrphanageParams>();
   const [orphanage, setOrphanage] = useState<Orphanage>();
   const [activeImageIndex, setActiveImageIndex] = useState(0);
-  
-  useEffect(()=>{
-    api.get(`orphanages/${params.id}`).then(response => {
+
+  useEffect(() => {
+    api.get(`orphanages/${params.id}`).then((response) => {
       setOrphanage(response.data);
-    })
+    });
   }, [params.id]);
-  
-  if(!orphanage){
-    return <p>Carregando...</p>
+
+  if (!orphanage) {
+    return <p>Carregando...</p>;
   }
 
   return (
     <div id="page-orphanage">
-      <Sidebar /> 
+      <Sidebar />
 
       <main>
         <div className="orphanage-details">
-          <img src={orphanage.images[activeImageIndex].url} alt={orphanage.name} />
+          <img
+            src={orphanage.images[activeImageIndex].url}
+            alt={orphanage.name}
+          />
 
           <div className="images">
-           {orphanage.images.map((image,index) => {
-             return (
-              <button 
-                key={image.id} 
-                className={activeImageIndex === index ? 'active' : ''} 
-                type="button" 
-                onClick={() => setActiveImageIndex(index)}
-              >
-                <img src={image.url} alt={orphanage.name} />
-              </button>
-             )
-           })}  
+            {orphanage.images.map((image, index) => {
+              return (
+                <button
+                  key={image.id}
+                  className={activeImageIndex === index ? 'active' : ''}
+                  type="button"
+                  onClick={() => setActiveImageIndex(index)}
+                >
+                  <img src={image.url} alt={orphanage.name} />
+                </button>
+              );
+            })}
           </div>
-          
+
           <div className="orphanage-details-content">
             <h1>{orphanage.name}</h1>
             <p>{orphanage.about}</p>
 
             <div className="map-container">
-              <Map 
+              <Map
                 center={[orphanage.latitude, orphanage.longitude]}
-                zoom={16} 
+                zoom={16}
                 style={{ width: '100%', height: 280 }}
                 dragging={false}
                 touchZoom={false}
@@ -81,16 +84,21 @@ export default function Orphanage() {
                 scrollWheelZoom={false}
                 doubleClickZoom={false}
               >
-                <TileLayer 
-                  url={`https://api.mapbox.com/styles/v1/mapbox/satellite-v9/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`} 
+                <TileLayer
+                  url={`https://api.mapbox.com/styles/v1/mapbox/satellite-v9/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
                 />
-                <Marker interactive={false} icon={happyMapIcon} position={[orphanage.latitude, orphanage.longitude]} />
+                <Marker
+                  interactive={false}
+                  icon={happyMapIcon}
+                  position={[orphanage.latitude, orphanage.longitude]}
+                />
               </Map>
 
               <footer>
-                <a 
+                <a
                   href={`https://www.google.com/maps/dir/?api=1&destination=${orphanage.latitude},${orphanage.longitude}`}
-                  target="_blank" rel="noopener noreferrer"
+                  target="_blank"
+                  rel="noopener noreferrer"
                 >
                   Ver rotas no Google Maps
                 </a>
@@ -120,7 +128,7 @@ export default function Orphanage() {
                   Não atendemos <br />
                   fim de semana
                 </div>
-              ) }
+              )}
             </div>
 
             {/* <button type="button" className="contact-button">
